a4886fa971b85fe403b90f61956b91e600c6a555
Chavier <javiercastrorodriguez2005@gmail.com>
2025-02-19 22:26:06 +0100

    Creacion del future builder

--- lib/screens/normal_screen.dart
+++ lib/screens/normal_screen.dart
@@ -1,8 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NormalScreen extends StatelessWidget {
   const NormalScreen({super.key});
 
+Future<Map<String, dynamic>> _cargarDatos() async {
+  final String response = await rootBundle.loadString('assets/comidas.json');
+  final data = json.decode(response);
+  return data['diario'];
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +29,19 @@ class NormalScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(''),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _cargarDatos(),
+        builder: (context, snapshot){
+          if(!snapshot.hasData){
+            return const Center(child: CircularProgressIndicator());
+          }
+          final datos = snapshot.data!;
+          return ListView(
+            children: [
+              
+            ],
+          );
+        }
       ),
     );
   }
