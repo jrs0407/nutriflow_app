6af3d5c7689c03de2a5032bfdfe09181bd5f10d7
Chavier <javiercastrorodriguez2005@gmail.com>
2025-02-19 18:24:01 +0100

    Creacion del normal Screen

--- lib/screens/normal_screen.dart
+++ lib/screens/normal_screen.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
 
 class NormalScreen extends StatelessWidget {
-  const NormalScreen({Key? key}) : super(key: key);
+  const NormalScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const Color primaryGreen = Color(0xFF2E7D32);
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: primaryGreen,
-        title: const Text(
-          'Pantalla principal',
-          style: TextStyle(color: Colors.white),
-        ),
+        title: const Text('Diario de Alimentación'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          '¡Bienvenido a la pantalla normal!',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Center(
+        child: Text(''),
       ),
     );
   }
