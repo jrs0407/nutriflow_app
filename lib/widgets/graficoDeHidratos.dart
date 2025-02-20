e0fb423ecebd823f34c2a5a50de5afc1f72fb336
Chavier <javiercastrorodriguez2005@gmail.com>
2025-02-20 20:14:33 +0100

    Mostrar grafico

--- lib/widgets/graficoDeHidratos.dart
+++ lib/widgets/graficoDeHidratos.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:nutriflow_app/models/ChartData.dart';
+
 class Graficodehidratos extends StatefulWidget {
   const Graficodehidratos({super.key});
 
@@ -17,38 +18,83 @@ class _GraficodehidratosState extends State<Graficodehidratos> {
   void initState() {
     super.initState();
     _cargarDatos();
-    }
+  }
 
-    Future<void> _cargarDatos() async {
-      final String response = await rootBundle.loadString('assets/comidas.json');
-      final Map<String, dynamic> data = json.decode(response)['diario'];
-  
+  Future<void> _cargarDatos() async {
+    final String response = await rootBundle.loadString('assets/comidas.json');
+    final Map<String, dynamic> data = json.decode(response)['diario'];
+
     double total_calorias = 0;
     double total_proteinas = 0;
     double total_grasas = 0;
     double total_hidratos = 0;
 
-data.forEach((key, value) {
-  total_hidratos += value['total_hidratos'] ?? 0;
-  total_calorias += value['total_calorias'] ?? 0;
-  total_proteinas += value['total_proteinas'] ?? 0;
-  total_grasas += value['total_grasas'] ?? 0;
-});
+    data.forEach((key, value) {
+      total_hidratos += value['total_hidratos'] ?? 0;
+      total_calorias += value['total_calorias'] ?? 0;
+      total_proteinas += value['total_proteinas'] ?? 0;
+      total_grasas += value['total_grasas'] ?? 0;
+    });
 
- if (total_hidratos == 0) {
+    if (total_hidratos == 0) {
       setState(() {
         chartData = [];
       });
       return;
     }
-  double porcentajeCalorias = (total_calorias / total_hidratos) * 100;
+    double porcentajeCalorias = (total_calorias / total_hidratos) * 100;
     double porcentajeGrasas = (total_grasas / total_hidratos) * 100;
     double porcentajeProteinas = (total_proteinas / total_hidratos) * 100;
     setState(() {
       chartData = [
-        Chartdata(x: 'Calorias', y:  double.parse(porcentajeCalorias.toStringAsFixed(1)), color: Colors.red),
-        Chartdata(x: 'Grasas', y: double.parse(porcentajeGrasas.toStringAsFixed(1)), color: Colors.green),
-        Chartdata(x: 'Proteinas', y: double.parse(porcentajeProteinas.toStringAsFixed(1)), color: Colors.blue),
+        Chartdata(
+            x: 'Calorias',
+            y: double.parse(porcentajeCalorias.toStringAsFixed(1)),
+            color: Colors.red),
+        Chartdata(
+            x: 'Grasas',
+            y: double.parse(porcentajeGrasas.toStringAsFixed(1)),
+            color: Colors.green),
+        Chartdata(
+            x: 'Proteinas',
+            y: double.parse(porcentajeProteinas.toStringAsFixed(1)),
+            color: Colors.blue),
       ];
     });
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gráfico de Hidratos de Carbono'),
+      ),
+      body: Center(
+        child: chartData.isEmpty
+            ? const CircularProgressIndicator()
+            : SfCircularChart(
+                title: ChartTitle(
+                    text:
+                        "Relación de Calorías, Grasas y Proteínas con Hidratos (%)"),
+                legend: Legend(
+                  isVisible: true,
+                  position: LegendPosition.bottom,
+                  overflowMode: LegendItemOverflowMode.wrap,
+                ),
+                series: <CircularSeries>[
+                  PieSeries<ChartData, String>(
+                    dataSource: chartData,
+                    xValueMapper: (ChartData data, _) => data.x,
+                    yValueMapper: (ChartData data, _) => data.y,
+                    pointColorMapper: (ChartData data, _) => data.color,
+                    dataLabelSettings: const DataLabelSettings(
+                      isVisible: true,
+                      labelPosition: ChartDataLabelPosition.outside,
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+}
