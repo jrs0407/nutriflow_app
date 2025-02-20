c7b935dab55b368975b12fc0f3120635386d5b33
Chavier <javiercastrorodriguez2005@gmail.com>
2025-02-20 12:04:44 +0100

    Comida card movido de la normal screen a un widget a parte para que alla mas orden

---
+++ lib/widgets/comidaCard.dart
@@ -0,0 +1,68 @@
+
+import 'package:flutter/material.dart';
+import 'package:nutriflow_app/models/comidas.dart';
+
+class Comidacard extends StatelessWidget {
+  const Comidacard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      
+    );
+  }
+}
+/*  return Card(
+                margin: const EdgeInsets.all(10),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "$titulo - ${seccionComida['total_calorias']} cal",
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      const SizedBox(height: 8),
+                      Column(
+                        children: comidas.map((comida) => Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 4.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            comida.nombre,
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                          Text(
+                                            comida.cantidad,
+                                            style: const TextStyle(
+                                                color: Colors.grey),
+                                          ),
+                                        ],
+                                      ),
+                                      Text(
+                                        "${comida.calorias} cal",
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.red),
+                                      ),
+                                      
+                                    ],
+                                  ),
+                                ))
+                            .toList(),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+              */
