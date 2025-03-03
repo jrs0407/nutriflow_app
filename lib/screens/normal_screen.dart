46381d9e88fba10ebff52cae4bf201510644afee
Chavier <javiercastrorodriguez2005@gmail.com>
2025-03-03 18:33:42 +0100

    Grafico añadido al normalScreen

--- lib/screens/normal_screen.dart
+++ lib/screens/normal_screen.dart
@@ -1,45 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:nutriflow_app/services/firebase_service.dart';
 import 'package:nutriflow_app/widgets/comidaCard.dart';
+import 'package:nutriflow_app/widgets/pie_chart_comidas.dart'; // Importa el gráfico
 
-class NormalScreen extends StatelessWidget {
+class NormalScreen extends StatefulWidget {
   const NormalScreen({super.key});
+
+  @override
+  State<NormalScreen> createState() => _NormalScreenState();
+}
+
+class _NormalScreenState extends State<NormalScreen> {
+  double totalCalorias = 0;
+  double totalProteinas = 0;
+  double totalGrasas = 0;
+
+  Future<void> _calcularTotales() async {
+    final comidas = await getComidas();
+    double calorias = 0;
+    double proteinas = 0;
+    double grasas = 0;
+
+    for (var comida in comidas) {
+      calorias += comida['calorias'].toDouble();
+      proteinas += comida['proteinas'].toDouble();
+      grasas += comida['grasas'].toDouble();
+    }
+
+    setState(() {
+      totalCalorias = calorias;
+      totalProteinas = proteinas;
+      totalGrasas = grasas;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _calcularTotales();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
-        title: const Row(
-          children: [
-            Icon(Icons.menu),
-            Spacer(),
-            Text(
-              'Diario',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Spacer(),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 20,
-                child: Text('NF',
-                    style:
-                        TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
-              ),
-            ),
-          ],
+        title: const Text(
+          'Diario',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              child: Text(
+                'JC',
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+              backgroundColor: Colors.white,
+              radius: 20,
+            ),
+          ),
+        ],
       ),
       body: FutureBuilder<List>(
         future: getComidas(),
         builder: (context, snapshot) {
-          print("Estado del snapshot: ${snapshot.connectionState}");
-
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           }
@@ -52,18 +79,58 @@ class NormalScreen extends StatelessWidget {
 
           final comidas = snapshot.data!;
 
-          return ListView.builder(
-            itemCount: comidas.length,
-            itemBuilder: (context, index) {
-              final comida = comidas[index];
-              return ComidaCard(
-                titulo: comida['nombre'],
-                cantidad: comida['cantidad'],
-                calorias: comida['calorias'].toInt(),
-                grasas: comida['grasas'].toInt(),
-                proteinas: comida['proteinas'].toInt(),
-              );
-            },
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                ListView.builder(
+                  shrinkWrap: true, 
+                  physics:
+                      const NeverScrollableScrollPhysics(), 
+                  itemCount: comidas.length,
+                  itemBuilder: (context, index) {
+                    final comida = comidas[index];
+                    return ComidaCard(
+                      titulo: comida['nombre'],
+                      cantidad: comida['cantidad'].toString(),
+                      calorias: comida['calorias'].toInt(),
+                      grasas: comida['grasas'].toInt(),
+                      proteinas: comida['proteinas'].toInt(),
+                    );
+                  },
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "Distribución de Macronutrientes",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          PieChartComidas(
+                            totalCalorias: totalCalorias,
+                            totalProteinas: totalProteinas,
+                            totalGrasas: totalGrasas,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
           );
         },
       ),
